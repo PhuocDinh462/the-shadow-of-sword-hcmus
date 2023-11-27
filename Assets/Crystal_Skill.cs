7268a788238fc1e3cfb2bf723320f71d0276ee19
zeuxi141 <74851601+zeuxi141@users.noreply.github.com>
2023-11-28 03:24:17 +0700

    TK: Crystal Explode

--- Assets/Crystal_Skill.cs
+++ Assets/Crystal_Skill.cs
@@ -8,6 +8,14 @@ public class Crystal_Skill : Skill
     [SerializeField] private GameObject crystalPrefab;
     private GameObject currentCrsytal;
 
+
+    [Header("Explosive Crystal")]
+    [SerializeField] private bool canExplode;
+
+    [Header("Moving Crystal")]
+    [SerializeField] private bool canMoveToEnemy;
+    [SerializeField] private float moveSpeed;
+
     public override void UseSkill()
     {
         base.UseSkill();
@@ -19,12 +27,16 @@ public class Crystal_Skill : Skill
 
             Crystal_Skill_Controller currentCrystalScript = currentCrsytal.GetComponent<Crystal_Skill_Controller>();
 
-            currentCrystalScript.SetupCrystal(crystalDuration);
+            currentCrystalScript.SetupCrystal(crystalDuration, canExplode, canMoveToEnemy, moveSpeed);
         }
         else
         {
+            Vector2 playerPos = player.transform.position; 
             player.transform.position = currentCrsytal.transform.position;
-            Destroy(currentCrsytal); 
+            
+            currentCrsytal.transform.position = playerPos;
+            currentCrsytal.GetComponent<Crystal_Skill_Controller>()?.FinishCrystal();
+
         }
     }
 
