e2573452b1560eb6d2071f28bb0d7cf747411bb2
zeuxi141 <74851601+zeuxi141@users.noreply.github.com>
2023-11-29 01:58:58 +0700

    TK: create Character stat inclue: dame, hp ...

--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -10,6 +10,7 @@ public class Entity : MonoBehaviour
     public EntityFX fx { get; private set; }
 
     public SpriteRenderer sr { get; private set; }
+    public CharacterStats stats { get; private set; }
     #endregion
 
 
@@ -41,6 +42,7 @@ public class Entity : MonoBehaviour
         anim = GetComponentInChildren<Animator>();
         rb = GetComponent<Rigidbody2D>();
         fx = GetComponent<EntityFX>();
+        stats = GetComponent<CharacterStats>();
     }
 
     protected virtual void Update() { }
--- Assets/Scripts/Player/PlayerAnimationTriggers.cs
+++ Assets/Scripts/Player/PlayerAnimationTriggers.cs
@@ -16,7 +16,10 @@ public class PlayerAnimationTriggers : MonoBehaviour
         foreach (var hit in colliders)
         {
             if (hit.GetComponent<Enemy>() != null)
+            {
                 hit.GetComponent<Enemy>().Damage();
+                hit.GetComponent<CharacterStats>().TakeDamge(player.stats.damage); 
+            }
         }
     }
 
