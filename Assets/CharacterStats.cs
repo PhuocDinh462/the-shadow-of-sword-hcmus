ab18bbb5102fba885f6ac2618700e39fa451294b
TienTran1504 <74586033+TienTran1504@users.noreply.github.com>
2023-11-29 18:45:39 +0700

    TT: Add Major stats and defensive stats

--- Assets/CharacterStats.cs
+++ Assets/CharacterStats.cs
@@ -1,14 +1,33 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterStats : MonoBehaviour
 {
+    [Header("Major stats")]
+    public Stat strength; // 1 point increase damage by 1 and crit.power  1%
+    public Stat agility;  // 1 increase evasion by 1% and crit.chance  1%
+    public Stat intelligence; // 1 increase magic damage 1 and magic resistance 3
+    public Stat vitality; // 1 increase health by 3-5 points
 
+    [Header("Offensive stats")]
     public Stat damage;
+    public Stat critChance;
+    public Stat critPower; // default value 150%
+
+
+    [Header("Defensive stats")]
     public Stat maxHealth;
-    public Stat strength;
+    public Stat armor;
+    public Stat evasion;
+    public Stat magicResistance;
+
+    [Header("Magic stats")]
+    public Stat fireDamage;
+    public Stat iceDamage;
+    public Stat lightingDamage;
+
+    public bool isIgnited; // make damge overtime
+    public bool isChilled; // reduce armor by 20%
+    public bool isShocked; // reduce accuracy by 20%
 
     [SerializeField] private int currentHealth;
 
@@ -21,8 +40,12 @@ public class CharacterStats : MonoBehaviour
 
     public virtual void DoDamage(CharacterStats _targetStats)
     {
+        if (TargetCanAvoidAttack(_targetStats)) return;
 
         int totalDamage = damage.GetValue() + strength.GetValue();
+
+
+        totalDamage = CheckTargetArmor(_targetStats, totalDamage);
 
         _targetStats.TakeDamge(totalDamage);
 
@@ -31,6 +54,8 @@ public class CharacterStats : MonoBehaviour
     public virtual void TakeDamge(int _damage)
     {
         currentHealth -= _damage;
+
+        Debug.Log(_damage);
 
         if (currentHealth < 0)
         {
@@ -42,5 +67,25 @@ public class CharacterStats : MonoBehaviour
     protected virtual void Die()
     {
         //throw new NotImplementedException();
+    }
+
+    private int CheckTargetArmor(CharacterStats _targetStats, int totalDamage)
+    {
+        totalDamage -= _targetStats.armor.GetValue();
+
+        totalDamage = Mathf.Clamp(totalDamage, 0, int.MaxValue);
+        return totalDamage;
+    }
+
+    private bool TargetCanAvoidAttack(CharacterStats _targetStats)
+    {
+        int totalEvasion = _targetStats.evasion.GetValue() + _targetStats.agility.GetValue();
+
+        if (Random.Range(0, 100) < totalEvasion)
+        {
+            Debug.Log("ATTACK AVOIDED");
+            return true;
+        }
+        return false;
     }
 }
