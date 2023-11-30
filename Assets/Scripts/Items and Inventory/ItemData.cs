f2d3839b1cd54bc6e37d4dfe850569b86e9b52d0
Phuoc Dinh <phuocdinh462@gmail.com>
2023-11-30 11:11:36 +0700

    HP: Equipment item

--- Assets/Scripts/Items and Inventory/ItemData.cs
+++ Assets/Scripts/Items and Inventory/ItemData.cs
@@ -1,7 +1,13 @@
 using UnityEngine;
 
+public enum ItemType {
+  Material,
+  Equipment
+}
+
 [CreateAssetMenu(fileName = "New Item Data", menuName = "Data/Item")]
 public class ItemData : ScriptableObject {
+  public ItemType itemType;
   public string itemName;
   public Sprite itemIcon;
 }
