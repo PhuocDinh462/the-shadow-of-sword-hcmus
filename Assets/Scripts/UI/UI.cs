8c46ee18698f1cb3d39f1da577db1fa1d358bb3b
TienTran1504 <74586033+TienTran1504@users.noreply.github.com>
2023-12-06 16:30:25 +0700

    TT: Switch UI (character, craft, skill tree, options) with hot key

--- Assets/Scripts/UI/UI.cs
+++ Assets/Scripts/UI/UI.cs
@@ -2,18 +2,40 @@ using UnityEngine;
 
 public class UI : MonoBehaviour {
   [SerializeField] private GameObject characterUI;
+  [SerializeField] private GameObject skillTreeUI;
+  [SerializeField] private GameObject craftUI;
+  [SerializeField] private GameObject optionsUI;
+
 
   public UI_ItemToolTip itemToolTip;
   public UI_StatToolTip statToolTip;
 
+
+
   // Start is called before the first frame update
   void Start() {
-
+    SwitchTo(null);
+    itemToolTip.gameObject.SetActive(false);
+    statToolTip.gameObject.SetActive(false);
   }
 
   // Update is called once per frame
   void Update() {
+    if(Input.GetKeyDown(KeyCode.C)){
+      SwitchWithKeyTo(characterUI);
+    }
 
+    if(Input.GetKeyDown(KeyCode.B)){
+      SwitchWithKeyTo(craftUI);
+    }
+
+    if(Input.GetKeyDown(KeyCode.K)){
+      SwitchWithKeyTo(skillTreeUI);
+    }
+
+    if(Input.GetKeyDown(KeyCode.O)){
+      SwitchWithKeyTo(optionsUI);
+    }
   }
 
   public void SwitchTo(GameObject _menu) {
@@ -22,5 +44,13 @@ public class UI : MonoBehaviour {
 
     if (_menu)
       _menu.SetActive(true);
+  }
+
+  public void SwitchWithKeyTo(GameObject _menu) {
+    if(_menu != null && _menu.activeSelf){
+      _menu.SetActive(false);
+      return;
+    }
+    SwitchTo(_menu);
   }
 }
