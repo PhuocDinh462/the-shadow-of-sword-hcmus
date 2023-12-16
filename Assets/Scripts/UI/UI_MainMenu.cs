edffaf620ace2784863893afb3db68cf6e17c44f
Phuoc Dinh <phuocdinh462@gmail.com>
2023-12-17 05:03:40 +0700

    HP: Add audio to MainMenu

--- Assets/Scripts/UI/UI_MainMenu.cs
+++ Assets/Scripts/UI/UI_MainMenu.cs
@@ -6,22 +6,27 @@ public class UI_MainMenu : MonoBehaviour {
   [SerializeField] private string sceneName = "MainScene";
   [SerializeField] private GameObject continueButton;
   [SerializeField] UI_FadeScreen fadeScreen;
+  [SerializeField] int bgmIndex = 5;
 
   private void Start() {
     if (!SaveManager.instance.HasSavedData())
       continueButton.SetActive(false);
+    AudioManager.instance.PlayBGM(bgmIndex);
   }
 
   public void ContinueGame() {
+    AudioManager.instance.PlaySFX(7);
     StartCoroutine(LoadSceneWithFadeEffect(1.5f));
   }
 
   public void NewGame() {
+    AudioManager.instance.PlaySFX(7);
     SaveManager.instance.DeleteSavedData();
     StartCoroutine(LoadSceneWithFadeEffect(1.5f));
   }
 
   public void ExitGame() {
+    AudioManager.instance.PlaySFX(7);
     Application.Quit();
   }
 
